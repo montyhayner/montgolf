fd6c55929853cfff7c5cebfc16b095a404538bec
Richard LaMont Hayner <montyhayner@outlook.com>
2026-05-29 23:48:24 -0400

    Fix PORT binding and restore dotenv

--- server.js
+++ server.js
@@ -2226,7 +2226,8 @@ cron.schedule("0 20 * * *", async () => {
 // ------------------------------
 // START SERVER
 // ------------------------------
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
