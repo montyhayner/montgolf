7bb8d8ebfbfba0c88778660f5c528d228ee77ef7
Richard LaMont Hayner <montyhayner@outlook.com>
2026-07-04 21:27:47 -0400

    fix mailer.js

--- services/mailer.js
+++ services/mailer.js
@@ -1,5 +1,7 @@
 // services/mailer.js
 
+const nodemailer = require("nodemailer");
+
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: process.env.EMAIL_PORT,
